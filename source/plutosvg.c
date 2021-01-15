75c83565ec893a77b0e5d5ebbf28dc9f949610a2
sammycage <sammycageagle@gmail.com>
2021-01-15 18:37:37 +0100

    update plutovg and parse_transform_list

--- source/plutosvg.c
+++ source/plutosvg.c
@@ -915,15 +915,9 @@ static int parse_transform_list(const element_t* e, int id, plutovg_matrix_t* ma
             break;
         case transform_type_rotate:
             if(count == 1)
-            {
                 plutovg_matrix_rotate(matrix, values[0]*PI/180.0);
-            }
             else
-            {
-                plutovg_matrix_translate(matrix, values[1], values[2]);
-                plutovg_matrix_rotate(matrix, values[0]*PI/180.0);
-                plutovg_matrix_translate(matrix, -values[1], -values[2]);
-            }
+                plutovg_matrix_rotate_translate(matrix, values[0]*PI/180.0, values[1], values[2]);
             break;
         case transform_type_scale:
             if(count == 1)
