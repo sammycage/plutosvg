e674e2a006ab5f8ec5939b224ca75eed428ac66a
sammycage <sammycageagle@gmail.com>
2024-08-20 18:22:24 +0100

    Use plutovg_color_t functions

--- source/plutosvg.c
+++ source/plutosvg.c
@@ -1,7 +1,6 @@
 #include "plutosvg.h"
 
 #include <stdint.h>
-#include <ctype.h>
 #include <float.h>
 #include <math.h>
 #include <stdio.h>
@@ -684,246 +683,20 @@ typedef struct {
     string_t id;
 } paint_t;
 
-typedef struct {
-    const char* name;
-    uint32_t value;
-} color_entry_t;
-
-static int color_entry_compare(const void* a, const void* b)
-{
-    const char* name = a;
-    const color_entry_t* entry = b;
-    return strcmp(name, entry->name);
-}
-
-static bool parse_color_component(const char** begin, const char* end, int* component)
-{
-    float value = 0;
-    if(!parse_float(begin, end, &value))
-        return false;
-    if(skip_delim(begin, end, '%'))
-        value *= 2.55f;
-    *component = lroundf(value);
-    *component = CLAMP(*component, 0.f, 255.f);
-    return true;
-}
-
 static bool parse_color_value(const char** begin, const char* end, color_t* color)
 {
     const char* it = *begin;
-    if(skip_delim(&it, end, '#')) {
-        const char* begin = it;
-        uint32_t value = 0;
-        while(it < end && isxdigit(*it)) {
-            const char cc = *it++;
-            unsigned int digit = 0;
-            if(cc >= '0' && cc <= '9')
-                digit = cc - '0';
-            else if(cc >= 'a' && cc <= 'f')
-                digit = 10 + cc - 'a';
-            else if(cc >= 'A' && cc <= 'F')
-                digit = 10 + cc - 'A';
-            value = value * 16 + digit;
-        }
-
-        int count = (int)(it - begin);
-        if(count != 6 && count != 3)
-            return false;
-        if(count == 3) {
-            value = ((value&0xf00) << 8) | ((value&0x0f0) << 4) | (value&0x00f);
-            value |= value << 4;
-        }
-
-        color->type = color_type_fixed;
-        color->value = value | 0xFF000000;
-    } else if(skip_string(&it, end, "currentColor")) {
+    if(skip_string(&it, end, "currentColor")) {
         color->type = color_type_current;
         color->value = 0xFF000000;
-    } else if(skip_string(&it, end, "rgb")) {
-        if(!skip_ws(&it, end)
-            || !skip_delim(&it, end, '(')
-            || !skip_ws(&it, end)) {
-            return false;
-        }
-
-        int red, green, blue;
-        if(!parse_color_component(&it, end, &red)
-            || !skip_ws_comma(&it, end)
-            || !parse_color_component(&it, end, &green)
-            || !skip_ws_comma(&it, end)
-            || !parse_color_component(&it, end, &blue)
-            || !skip_ws(&it, end)
-            || !skip_delim(&it, end, ')')) {
-            return false;
-        }
-
-        color->type = color_type_fixed;
-        color->value = (0xFF000000 | red << 16 | green << 8 | blue);
     } else {
-        static const color_entry_t colormap[] = {
-            {"aliceblue", 0xF0F8FF},
-            {"antiquewhite", 0xFAEBD7},
-            {"aqua", 0x00FFFF},
-            {"aquamarine", 0x7FFFD4},
-            {"azure", 0xF0FFFF},
-            {"beige", 0xF5F5DC},
-            {"bisque", 0xFFE4C4},
-            {"black", 0x000000},
-            {"blanchedalmond", 0xFFEBCD},
-            {"blue", 0x0000FF},
-            {"blueviolet", 0x8A2BE2},
-            {"brown", 0xA52A2A},
-            {"burlywood", 0xDEB887},
-            {"cadetblue", 0x5F9EA0},
-            {"chartreuse", 0x7FFF00},
-            {"chocolate", 0xD2691E},
-            {"coral", 0xFF7F50},
-            {"cornflowerblue", 0x6495ED},
-            {"cornsilk", 0xFFF8DC},
-            {"crimson", 0xDC143C},
-            {"cyan", 0x00FFFF},
-            {"darkblue", 0x00008B},
-            {"darkcyan", 0x008B8B},
-            {"darkgoldenrod", 0xB8860B},
-            {"darkgray", 0xA9A9A9},
-            {"darkgreen", 0x006400},
-            {"darkgrey", 0xA9A9A9},
-            {"darkkhaki", 0xBDB76B},
-            {"darkmagenta", 0x8B008B},
-            {"darkolivegreen", 0x556B2F},
-            {"darkorange", 0xFF8C00},
-            {"darkorchid", 0x9932CC},
-            {"darkred", 0x8B0000},
-            {"darksalmon", 0xE9967A},
-            {"darkseagreen", 0x8FBC8F},
-            {"darkslateblue", 0x483D8B},
-            {"darkslategray", 0x2F4F4F},
-            {"darkslategrey", 0x2F4F4F},
-            {"darkturquoise", 0x00CED1},
-            {"darkviolet", 0x9400D3},
-            {"deeppink", 0xFF1493},
-            {"deepskyblue", 0x00BFFF},
-            {"dimgray", 0x696969},
-            {"dimgrey", 0x696969},
-            {"dodgerblue", 0x1E90FF},
-            {"firebrick", 0xB22222},
-            {"floralwhite", 0xFFFAF0},
-            {"forestgreen", 0x228B22},
-            {"fuchsia", 0xFF00FF},
-            {"gainsboro", 0xDCDCDC},
-            {"ghostwhite", 0xF8F8FF},
-            {"gold", 0xFFD700},
-            {"goldenrod", 0xDAA520},
-            {"gray", 0x808080},
-            {"green", 0x008000},
-            {"greenyellow", 0xADFF2F},
-            {"grey", 0x808080},
-            {"honeydew", 0xF0FFF0},
-            {"hotpink", 0xFF69B4},
-            {"indianred", 0xCD5C5C},
-            {"indigo", 0x4B0082},
-            {"ivory", 0xFFFFF0},
-            {"khaki", 0xF0E68C},
-            {"lavender", 0xE6E6FA},
-            {"lavenderblush", 0xFFF0F5},
-            {"lawngreen", 0x7CFC00},
-            {"lemonchiffon", 0xFFFACD},
-            {"lightblue", 0xADD8E6},
-            {"lightcoral", 0xF08080},
-            {"lightcyan", 0xE0FFFF},
-            {"lightgoldenrodyellow", 0xFAFAD2},
-            {"lightgray", 0xD3D3D3},
-            {"lightgreen", 0x90EE90},
-            {"lightgrey", 0xD3D3D3},
-            {"lightpink", 0xFFB6C1},
-            {"lightsalmon", 0xFFA07A},
-            {"lightseagreen", 0x20B2AA},
-            {"lightskyblue", 0x87CEFA},
-            {"lightslategray", 0x778899},
-            {"lightslategrey", 0x778899},
-            {"lightsteelblue", 0xB0C4DE},
-            {"lightyellow", 0xFFFFE0},
-            {"lime", 0x00FF00},
-            {"limegreen", 0x32CD32},
-            {"linen", 0xFAF0E6},
-            {"magenta", 0xFF00FF},
-            {"maroon", 0x800000},
-            {"mediumaquamarine", 0x66CDAA},
-            {"mediumblue", 0x0000CD},
-            {"mediumorchid", 0xBA55D3},
-            {"mediumpurple", 0x9370DB},
-            {"mediumseagreen", 0x3CB371},
-            {"mediumslateblue", 0x7B68EE},
-            {"mediumspringgreen", 0x00FA9A},
-            {"mediumturquoise", 0x48D1CC},
-            {"mediumvioletred", 0xC71585},
-            {"midnightblue", 0x191970},
-            {"mintcream", 0xF5FFFA},
-            {"mistyrose", 0xFFE4E1},
-            {"moccasin", 0xFFE4B5},
-            {"navajowhite", 0xFFDEAD},
-            {"navy", 0x000080},
-            {"oldlace", 0xFDF5E6},
-            {"olive", 0x808000},
-            {"olivedrab", 0x6B8E23},
-            {"orange", 0xFFA500},
-            {"orangered", 0xFF4500},
-            {"orchid", 0xDA70D6},
-            {"palegoldenrod", 0xEEE8AA},
-            {"palegreen", 0x98FB98},
-            {"paleturquoise", 0xAFEEEE},
-            {"palevioletred", 0xDB7093},
-            {"papayawhip", 0xFFEFD5},
-            {"peachpuff", 0xFFDAB9},
-            {"peru", 0xCD853F},
-            {"pink", 0xFFC0CB},
-            {"plum", 0xDDA0DD},
-            {"powderblue", 0xB0E0E6},
-            {"purple", 0x800080},
-            {"rebeccapurple", 0x663399},
-            {"red", 0xFF0000},
-            {"rosybrown", 0xBC8F8F},
-            {"royalblue", 0x4169E1},
-            {"saddlebrown", 0x8B4513},
-            {"salmon", 0xFA8072},
-            {"sandybrown", 0xF4A460},
-            {"seagreen", 0x2E8B57},
-            {"seashell", 0xFFF5EE},
-            {"sienna", 0xA0522D},
-            {"silver", 0xC0C0C0},
-            {"skyblue", 0x87CEEB},
-            {"slateblue", 0x6A5ACD},
-            {"slategray", 0x708090},
-            {"slategrey", 0x708090},
-            {"snow", 0xFFFAFA},
-            {"springgreen", 0x00FF7F},
-            {"steelblue", 0x4682B4},
-            {"tan", 0xD2B48C},
-            {"teal", 0x008080},
-            {"thistle", 0xD8BFD8},
-            {"tomato", 0xFF6347},
-            {"turquoise", 0x40E0D0},
-            {"violet", 0xEE82EE},
-            {"wheat", 0xF5DEB3},
-            {"white", 0xFFFFFF},
-            {"whitesmoke", 0xF5F5F5},
-            {"yellow", 0xFFFF00},
-            {"yellowgreen", 0x9ACD32}
-        };
-
-        char name[24];
-        int length = 0;
-        while(it < end && length < 24 && isalpha(*it))
-            name[length++] = tolower(*it++);
+        plutovg_color_t value;
+        int length = plutovg_color_parse(&value, it, end - it);
         if(length == 0)
             return false;
-        name[length] = 0;
-
-        color_entry_t* entry = bsearch(name, colormap, sizeof(colormap) / sizeof(color_entry_t), sizeof(color_entry_t), color_entry_compare);
-        if(entry == NULL)
-            return false;
         color->type = color_type_fixed;
-        color->value = entry->value | 0xFF000000;
+        color->value = plutovg_color_to_argb32(&value);
+        it += length;
     }
 
     *begin = it;
@@ -1900,12 +1673,9 @@ static element_t* resolve_href(const plutosvg_document_t* document, const elemen
 
 static plutovg_color_t convert_color(const color_t* color)
 {
-    uint8_t a = ((color->value >> 24) & 0xff);
-    uint8_t r = ((color->value >> 16) & 0xff);
-    uint8_t g = ((color->value >> 8) & 0xff);
-    uint8_t b = ((color->value >> 0) & 0xff);
-
-    return PLUTOVG_MAKE_COLOR(r / 255.f, g / 255.f, b / 255.f, a / 255.f);
+    plutovg_color_t value;
+    plutovg_color_init_argb32(&value, color->value);
+    return value;
 }
 
 static plutovg_color_t resolve_current_color(const render_context_t* context, const element_t* element)
